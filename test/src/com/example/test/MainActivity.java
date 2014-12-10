836b088e4392ae6b93a60cd1dd84c66278b59112
mreyeslo1 <mreyeslo@gmu.edu>
2014-12-09 23:51:41 -0500

    Update MainActivity.java

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -13,10 +13,10 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-
+//main 
 public class MainActivity extends Activity {
 	TextView txt1,txt2;
-	
+	//initialize all classes 
 	String str=null;
 	byte[] send_data = new byte[1024];
 	byte[] receiveData = new byte[1024];
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
         StrictMode.setThreadPolicy(policy); 
      
         setContentView(R.layout.activity_main);
-        
+        //text boxes 
         txt1   = (TextView)findViewById(R.id.textView1); 
         txt2   = (TextView)findViewById(R.id.textView9); 
         
@@ -43,8 +43,10 @@ public class MainActivity extends Activity {
         bt5 = (Button) findViewById(R.id.but5);
         bt6 = (Button) findViewById(R.id.but6);
         
+        //function for classes 
         bt1.setOnClickListener(new View.OnClickListener(){             
-        	public void onClick(View v) {                 
+        	public void onClick(View v) {   
+        		//send string str to udp server 
         		str="forward\0";
 				txt1.setText(str);
 				try {
@@ -129,25 +131,29 @@ public class MainActivity extends Activity {
 }
 	
 
-
+//functino that is responible for UDP communication 
 public void client() throws IOException{
 	  
-	 
+	 //intialize sockets for UDP 
 	 DatagramSocket client_socket = new DatagramSocket(8888);
     InetAddress IPAddress =  InetAddress.getByName("10.0.2.2"); 
-
+	//loop will send the STR and recieve 
    while (true)
    {
    	send_data = str.getBytes();
 //   	System.out.println("Type Something (q or Q to quit): ");
-
+//send the string 
        DatagramPacket send_packet = new DatagramPacket(send_data,str.length(), IPAddress, 8888);
        client_socket.send(send_packet); 
        
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		
+		//recieves the string 
 		client_socket.receive(receivePacket);
 		modifiedSentence = new String(receivePacket.getData());
+		//confrim that we recieved 
 		System.out.println("FROM SERVER:" + modifiedSentence);
+		//changes the text box 2
 		txt2.setText(modifiedSentence);
 	   client_socket.close();
 
